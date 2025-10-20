41d610b22ce2cd6aadfd143edae0b6b635d09eba
PhamNgocSonTech <phamngocson7a1@gmail.com>
2025-10-20 20:11:40 +0700

    [ADD]: create BlogList component with initial layout and styling

---
+++ src/views/BlogList.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <div class="main__container">
+    <div class="mt-60 text-center">
+      <h3 class="font-bold text-8xl text-(--primary-color)">From the blog </h3>
+      <p class="text-4xl mt-4 text-(--desc-color)">Learn how to grow your business with our expert advice.</p>
+    </div>
+  </div>
+
+</template>
+
+<style scoped></style>
